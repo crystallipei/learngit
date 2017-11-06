b55370242d1ad3025aae3e55e0d1a8d2326c23ad
bl2lp <1083066125@qq.com>
2017-11-06 09:42:07 +0800

    提交

--- cesium-master/example/js/main.js
+++ cesium-master/example/js/main.js
@@ -1,25 +1,29 @@
 /*global require*/
 // require in the complete Cesium object and reassign it globally.
 // This is meant for use with the Almond loader.
-var developMode=true;
-if(developMode){
-    require.config({
-       baseUrl:'../Source'
-    });
-}else{
-require.config({
-    paths:{
-    'Cesium':'../../Build/Cesium/Cesium'
-    },
-    shim:{
-        Cesium:{
-            exports:'Cesium'
-        }
+
+(function() {
+    'use strict';
+    var developMode=true;
+    if(developMode){
+        require.config({
+            baseUrl:'../Source'
+        });
+    }else{
+        require.config({
+            paths:{
+                'Cesium':'../../Build/Cesium/Cesium'
+            },
+            shim:{
+                Cesium:{
+                    exports:'Cesium'
+                }
+            }
+        });
     }
-});
-}
-if(typeof Cesium !=='undefined'){
-    onload(Cesium);
-}else if(typeof require==='function'){
-    require(['Cesium'],onload);
-}
+    if(typeof Cesium !=='undefined'){
+        onload(Cesium);
+    }else if(typeof require==='function'){
+        require(['Cesium'],onload);
+    }
+})();
